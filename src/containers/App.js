4931927924b786625edc92d3a08b59c5fc0a2576
Michael Purcella <mjpurcella@gmail.com>
2021-03-15 14:04:24 -0700

    Added Error Component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 class App extends Component {
@@ -36,7 +37,9 @@ class App extends Component {
                     <h1 className='f-subheadline ls2 tracked near-black ma2'>Monster Friends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList monsters={filteredMonsters}/>
+                        <ErrorBoundary>
+                            <CardList monsters={filteredMonsters}/>
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
             );
